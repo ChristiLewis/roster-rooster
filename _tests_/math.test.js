f58b4ee56f7642cae5e692c64b8f9c76e79d7c68
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-03-06 22:22:13 -0800

    change to new branch

---
+++ _tests_/math.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
+const math = require( './src/math' );
+
+test('checks to see if 2 + 3 = 5', () => {
+    expect(math.add(2,3)), toBe(5);
+})
+
+}
