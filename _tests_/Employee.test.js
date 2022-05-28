82b3e81c236f5b9f1fa8ae78144c4abb1b8998bd
Christi Lewis Cvijanovic <95652938+ChristiLewis@users.noreply.github.com>
2022-05-28 07:33:06 -0700

    passed tests now fail

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,18 +1,16 @@
-const { it, expect } = require('@jest/globals');
-const { getMaxListeners } = require('process');
 const Employee = require('../lib/Employee');
 
 //PARENT CLASS
-describe('Employee', () => {
-    it('should creat an object with a name, id, and email if provided correct inputs', () => {
-        const employee = new Employee('Chris', 0, 'chris@gmail.com')
-    })
-
-    expect(employee.name).toEqual('Chris');
-    expect(employee.id).toEqual(0);
-    expect(employee.email('Chris@gmail.com'));
+test('should create an object with a name, id, and email if provided correct inputs', () => {
+ 
+    const emp = new Employee(
+        'Chris',
+        1,
+        'chris@gmail.com'
+    )
 });
 
-// getRole() // Returns Employee
+
+
 
 
